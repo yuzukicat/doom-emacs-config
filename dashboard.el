94b7cd77b61d25bf03b7e536c329ce404fee9eea
yuzuki.cat <yuzuki.cat@kamisu66.com>
2023-02-14 20:58:16 +0900

    add golang lsp mode && lemon banner

--- dashboard.el
+++ dashboard.el
@@ -45,3 +45,28 @@
       ;;<<doom-ascii-banner>>
       )))
 (add-hook! 'doom-init-ui-hook #'ginshio/doom-init-ui-misc)
+
+(defun my/dashboard-draw-ascii-banner-fn ()
+  (let* ((banner
+          '("██╗░░░░░███████╗███╗░░░███╗░█████╗░███╗░░██╗"
+            "██║░░░░░██╔════╝████╗░████║██╔══██╗████╗░██║"
+            "██║░░░░░█████╗░░██╔████╔██║██║░░██║██╔██╗██║"
+            "██║░░░░░██╔══╝░░██║╚██╔╝██║██║░░██║██║╚████║"
+            "███████╗███████╗██║░╚═╝░██║╚█████╔╝██║░╚███║"
+            "╚══════╝╚══════╝╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝"
+            ""
+            "                Emacs Master                "
+            ""))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat
+                 line (make-string (max 0 (- longest-line (length line)))
+                                   32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-ascii-banner-fn #'my/dashboard-draw-ascii-banner-fn)
